726c03fb4eb71ce987f87432200e86f8963a17f5
cesar jesus leon martin <cesilius98@gmail.com>
2022-05-26 11:09:21 +0200

    Feature: acabada implementacion de la clase ListManager

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
 
+        SuperList listaEnteros = new SuperList<Integer>();
+
+        listaEnteros.addElement(3);
+        listaEnteros.addElement(4);
+        listaEnteros.addElement(5);
+        listaEnteros.addElement(6);
+
     }
 }
--- src/com/company/SuperList.java
+++ src/com/company/SuperList.java
@@ -2,27 +2,29 @@ package com.company;
 
 import java.util.Iterator;
 
-public class SuperList extends ListManager {
+public class SuperList<T> extends ListManager<T>  {
 
     @Override
-    public void addElement(Object element) {
+    public void addElement(T element) {
+
         list.add(element);
     }
 
     @Override
-    public Object removeElement(int position) {
+    public T removeElement(int position) {
+
         return list.remove(position);
     }
 
     @Override
     public void showElements() {
 
-        Iterator<Object> recorrido = list.iterator();
+        Iterator<T> recorrido = list.iterator();
         System.out.println("Super lista -> [");
 
         while(recorrido.hasNext()) {
-            Object elemento = recorrido.next();
-            System.out.println(elemento);
+            T elemento = recorrido.next();
+            System.out.print(elemento+" ");
         }
 
         System.out.println("]");
@@ -32,5 +34,14 @@ public class SuperList extends ListManager {
     @Override
     public void showReversedElements() {
 
+        System.out.println("Super List Reverse -> [");
+
+        for (int i = list.size(); i >= 0; i--) {
+
+            System.out.print(list.get(i)+" ");
+
+        }
+
+        System.out.println("]");
     }
 }
